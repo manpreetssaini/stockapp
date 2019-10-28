3d80ca5d0957b53dad5c97683746123416686c8c
manpreet saini <manpreet_saini92@hotmail.com>
2019-10-27 21:45:51 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+
+// set handlebars middleware
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there, this is other stuff"
+
+
+//set handlebar routes
+app.get('/', function (req, res) {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
