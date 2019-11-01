385beaf34c5f86f11054b5bf395762755bab9d44
manpreet saini <manpreet_saini92@hotmail.com>
2019-10-31 21:04:32 -0400

    added post request

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require('express');
 const path = require('path');
 const exphbs  = require('express-handlebars');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
+//use bodyparser middleware
+
+app.use(bodyParser.urlencoded({extended: false}));
+
 // api key pk_5f805ca504d74f89a9658b2d1f36a56d
 //create call api function
 
-function callApi(finishedAPI) {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_5f805ca504d74f89a9658b2d1f36a56d', { json: true }, (err, res, body) => {
+function callApi(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_5f805ca504d74f89a9658b2d1f36a56d', { json: true }, (err, res, body) => {
     if (err) {return (err);}
     if (res.statusCode === 200){
         finishedAPI(body);
@@ -27,14 +33,26 @@ app.set('view engine', 'handlebars');
 
 
 
-//set handlebar routes
+//set handlebar index GET routes
 app.get('/', function (req, res) {
     callApi(function(doneAPI)  {
             res.render('home', {
             stock: doneAPI
         });
-    });
+    }, "fb");
 });
+
+
+//set handlebar index POST routes
+app.post('/', function (req, res) {
+    callApi(function(doneAPI)  {
+            //posted_stuff = req.body.stock_ticker;
+            res.render('home', {
+            stock: doneAPI
+        });
+    }, req.body.stock_ticker);
+});
+
 
 // create about page route
 
