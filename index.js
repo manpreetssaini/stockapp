a6720657be926812b33bdf3aa67bc65a51e3112c
manpreet saini <manpreet_saini92@hotmail.com>
2019-10-31 20:38:02 -0400

    added functionality using api

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const path = require('path');
 const exphbs  = require('express-handlebars');
+const request = require('request');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// api key pk_5f805ca504d74f89a9658b2d1f36a56d
+//create call api function
 
+function callApi(finishedAPI) {
+    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_5f805ca504d74f89a9658b2d1f36a56d', { json: true }, (err, res, body) => {
+    if (err) {return (err);}
+    if (res.statusCode === 200){
+        finishedAPI(body);
+        };
+    });
+};
 
 // set handlebars middleware
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const otherstuff = "hello there, this is other stuff i added"
+
 
 
 //set handlebar routes
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: otherstuff
+    callApi(function(doneAPI)  {
+            res.render('home', {
+            stock: doneAPI
+        });
     });
 });
 
